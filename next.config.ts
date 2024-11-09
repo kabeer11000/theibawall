8c5be4fe4aa5076bfd8aadda3332ee5e75379d90
kabeer11000 <kabeer11000@gmail.com>
2024-11-09 15:09:26 +0500

    hehe

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
